0a69a9b1e257f8eb2ec7b64c1b1a7166a5f256a7
YuriiDzyuba <iurii11022@gmail.com>
2022-04-16 18:27:35 +0300

    added redis env variable

--- jwt-service/src/config/config.ts
+++ jwt-service/src/config/config.ts
@@ -1,5 +1,7 @@
 export const mongoDbUrl =
   process.env.MONGO_URL || 'mongodb://localhost:27017/jwt-api';
+export const redisUrl =
+  process.env.REDIS_URL || 'redis://redis:6379';
 export const mongoDbUser =
   process.env.MONGO_INITDB_ROOT_USERNAME || 'mongoUserName';
 export const mongoDbPassword =
