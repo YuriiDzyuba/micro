ec7421e484f44f8910bdca86b747bc7bcd2b1b51
YuriiDzyuba <iurii11022@gmail.com>
2022-05-14 11:27:14 +0300

    added topBar module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { environment } from "../environments/environment";
 import { EffectsModule } from "@ngrx/effects";
+import {TopBarModule} from "./topBar/topBar.module";
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { EffectsModule } from "@ngrx/effects";
       logOnly: environment.production, // Restrict extension to log-only mode
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
     }),
+    TopBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/auth/store/selectors.ts
+++ frontend/src/app/auth/store/selectors.ts
@@ -2,6 +2,7 @@ import { createSelector } from "@ngrx/store";
 import { AppStateInterface } from "../../shared/types/appState.interface";
 import { AuthStateInterface } from "../types/authState.interface";
 import { ValidationErrorsInterface } from "../../shared/types/validationErrors.interface";
+import { CurrentUserInterface } from "../../shared/types/currentUser.interface";
 
 export const authFeatureSelector = (state: AppStateInterface):AuthStateInterface => state.auth
 
@@ -13,4 +14,19 @@ export const isSubmittingSelector = createSelector(
 export const validationErrorsSelector = createSelector(
   authFeatureSelector,
   (authState: AuthStateInterface): ValidationErrorsInterface => authState.validationErrors
+)
+
+export const isLoggedInSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): boolean => authState.isLoggedIn
+)
+
+export const isAnonymousSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): boolean => authState.isLoggedIn === false
+)
+
+export const currentUserSelector = createSelector(
+  authFeatureSelector,
+  (authState: AuthStateInterface): CurrentUserInterface => authState.currentUser
 )
