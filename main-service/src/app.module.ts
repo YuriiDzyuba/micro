6e9b1629bf1ce602335d37d1f393309199cb7826
YuriiDzyuba <iurii11022@gmail.com>
2022-05-24 23:30:50 +0300

    added config module and set up first settings

--- main-service/src/app.module.ts
+++ main-service/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {User} from "../../jwt-service/src/user.module/entity/user.entity";
+import {EmailVerification} from "../../jwt-service/src/user.module/entity/emailVerification.entity";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'postgres'>('TYPEORM_CONNECTION'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        host: config.get<string>('TYPEORM_HOST'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity(.ts,.js)'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: false,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
