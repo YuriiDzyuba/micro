98f674f1ebdfd8734288fe5754ac600f7851a2f1
YuriiDzyuba <iurii11022@gmail.com>
2022-05-27 15:32:05 +0300

    Add dtos for account

---
+++ main-service/src/modules/account.module/dto/update.account.dto.ts
@@ -0,0 +1,20 @@
+import { Account } from '../types/account.type';
+import {ApiProperty} from "@nestjs/swagger";
+import {IsNotEmpty, MaxLength, MinLength} from "class-validator";
+
+export class UpdateAccountDto implements Pick<Account, 'name' | 'title' | 'description'> {
+    @ApiProperty({ example: 'Pol', description: 'account name' })
+    @MinLength(2)
+    @MaxLength(32)
+    name: string | null;
+
+    @ApiProperty({ example: 'My new account', description: 'account title' })
+    @MinLength(2)
+    @MaxLength(128)
+    title: string | null;
+
+    @ApiProperty({ example: 'description', description: 'account description' })
+    @MinLength(16)
+    @MaxLength(1024)
+    description: string | null;
+}
